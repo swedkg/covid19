147729a28479856fdf66cf30bbeba03e67907bf6
Dimitrios Giannopoulos <dimitrios.giannopoulos@gmail.com>
2020-04-16 21:41:57 +0200

    countries regional codes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var vlSpec = "spec.vg.json";
-var country = "World";
+var currentLocation = "World";
 var locationsList;
+var countriesCodes;
 
 function filteredLocation(data) {
   return data.filter((item) => {
-    return item.location == country;
+    return item.location == currentLocation;
   });
 }
 
 function updateData(res, data) {
   let dt = filteredLocation(data);
-  // console.log(country, dt);
 
   var changeSet = vega
     .changeset()
@@ -24,28 +24,54 @@ function updateData(res, data) {
 
 var locationSelect = document.querySelector("#location-select");
 
+d3.csv("countries_with_regional_codes.csv").then((data) => {
+  countriesCodes = data;
+});
+
 vegaEmbed("#vis", vlSpec, { actions: false }).then((res) => {
   d3.csv("https://covid.ourworldindata.org/data/ecdc/full_data.csv").then(
-    (data) => {
-      locationsList = [...new Set(data.map((a) => a.location))];
+    (covidData) => {
+      // filter out unique locations
+      locationsList = [...new Set(covidData.map((a) => a.location))];
+
+      var aggregatedData = [];
+
+      for (let l = 0; l < locationsList.length; l++) {
+        let location = locationsList[l];
+
+        codes = countriesCodes.filter((c) => {
+          return c.name == location || c.name.includes(location);
+        })[0];
+
+        var cntr = [];
+        cntr = covidData.filter((c) => {
+          return c.location == location;
+        });
+
+        if (codes)
+          cntr.forEach((c) => {
+            c.region = codes.region;
+            c.regionCode = codes["region-code"];
+            c.subRegion = codes["sub-region"];
+            c.subRegionCode = codes["sub-region-code"];
+          });
+        aggregatedData = aggregatedData.concat(cntr);
+      }
 
       locationsList = locationsList.filter((i) => {
         return i != "World";
       });
 
-      // add country options to Select element
+      // add options to Select element
       for (var i in locationsList) {
         locationSelect.add(new Option(locationsList[i], locationsList[i]));
       }
 
-      updateData(res, data);
+      updateData(res, aggregatedData);
 
       locationSelect.addEventListener("change", (event) => {
-        country = event.target.value;
-        updateData(res, data);
-
-        // const title = document.querySelector(".title");
-        // title.textContent = `Displaying data for ${event.target.value}`;
+        currentLocation = event.target.value;
+        updateData(res, aggregatedData);
       });
     } // end of loading csv promise
   );
